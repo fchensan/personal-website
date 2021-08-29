ffdb9340fbc9ba8642ee4e3ef45932bd64da7d41
fchensan <chensanfrancisco@gmail.com>
2021-08-29 17:27:14 +0800

    Add Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,23 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GOOGLE_TRACKING_ID, // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
